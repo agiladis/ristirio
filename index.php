d5c7bcfbec5b27572408e8e293a0b6ec5555f2f8
agil adi s <66274213+agiladis@users.noreply.github.com>
2021-04-04 14:14:40 +0700

    Push File

---
+++ index.php
@@ -0,0 +1,152 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Risti & Rio</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cedeen.netlify.app/font-awesome-5-pro/css/all.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>
+    <header>
+    </header>
+    <main>
+        <div id="content">
+            <div class="landing-page">
+                <div class="image-holder"></div>
+                <div class="landpage-caption text-center pt-3">
+                    <p class="text-secondary text font-italic">The Wedding Of :</p>
+                    <p class="font-alexBrush m-0 text-size-xl">Risti</p>
+                    <h1 class="font-alexBrush m-0">&</h1>
+                    <p class="font-alexBrush m-0 text-size-xl">Rio</p>
+                </div> <!-- end landpage-caption -->
+                <div class="text-center mt-3">
+                    <img class="w-25" src="./img/leaf.png" alt="">
+                </div>
+            </div> <!-- end landing-page -->
+            <div class=" container intro-person mt-4">
+                <div class="hadith col-md-8">
+                    <p>"Bismillahhirrahmanirrahim"</p>
+                    <p>
+                        Maha Suci Allah SWT
+                        yang telah menciptakan makhluk-Nya berpasang-pasangan
+                        perkenankan kami melaksanakan syariat agamamu mengikuti sunnah Rasul-mu, membentuk keluarga Sakinah Mawadah Warahmah.
+                    </p>
+                </div> <!-- end hadith -->
+                <div class="container-couple">
+                    <div class="couple-half">
+                        <div class="photo-holder">
+                            <img src="./img/_MG_3158.jpg" alt="">
+                        </div> <!-- end photo-holder -->
+                        <div class="desc-groom">
+                            <h1 class="font-alexBrush m-0">Risti</h1>
+                            <p class="text-secondary">Risti Rofiatun Nissa</p>
+                            <p>
+                                <small><i>Putri Pertama Dari</i></small>
+                                <br>
+                                Bapak Sumardani &
+                                <br>
+                                Ibu Uti Lutfiyah
+                            </p>
+                        </div> <!-- end desc-groom -->
+                    </div> <!-- end couple-half -->
+                    <div class="m-auto">
+                        <h1 class="font-alexBrush">- & -</h1>
+                    </div>
+                    <div class="couple-half text-right">
+                        <div class="desc-groom">
+                            <h1 class="font-alexBrush m-0">Rio</h1>
+                            <p class="text-secondary">Rio Mardani</p>
+                            <p>
+                                <small><i>Putra Kedua Dari</i></small>
+                                <br>
+                                Bapak Sumaryo &
+                                <br>
+                                Ibu Rakiyem
+                            </p>
+                        </div> <!-- end desc-groom -->
+                        <div class="photo-holder">
+                            <img src="./img/_MG_3161.jpg" alt="">
+                        </div> <!-- end photo-holder -->
+                    </div> <!-- end couple-half -->
+                </div> <!-- end container-couple -->
+            </div> <!-- end intro-person -->
+            <div class="container schedule-container">
+                <div class="hadith col-md-8">
+                    <p>
+                        Dan di antara tanda-tanda kekuasaan-Nya ialah Dia menciptakan untukmu istri-istri dari
+                        jenismu sendiri, supaya kamu cenderung dan merasa tentram kepadanya, dan dijadikan-Nya
+                        diantaramu rasa kasih dan sayang. Sesungguhnya pada demikian itu benar-benar terdapat
+                        tanda-tanda bagi kaum yang berfikir.
+                    </p>
+                    <p class="font-weight-bold">- Surat Ar-Rum Ayat 21 -</p>
+                </div> <!-- end hadith -->
+                <div class="schedule">
+                    <div class="akad">
+                        <h1 class="font-alexBrush m-0 text-center">Akad Nikah</h1>
+                        <div class="detail-akad">
+                            <div class="detail-time">
+                                <section>
+                                    <p>10.00 - Selesai</p>
+                                </section>
+                                <section>
+                                    <p>Wedding Ceremony</p>
+                                </section>
+                            </div> <!-- end detail-time -->
+                            <div class="detail-date">
+                                <h1 class="font-weight-bold">MEI</h1>
+                                <h1 class="font-weight-bold">30</h1>
+                                <h1 class="font-weight-bold">2021</h1>
+                            </div> <!-- end detail-date -->
+                            <div class="detail-place">
+                                <section>
+                                    <p>Lap. Serbaguna</p>
+                                </section>
+                                <section>
+                                    <p>Bekasi</p>
+                                </section>
+                            </div> <!-- end detail-place -->
+                        </div> <!-- end detail-akad -->
+                    </div> <!-- end akad -->
+                    <div class="akad">
+                        <h1 class="font-alexBrush m-0 text-center">Resepsi</h1>
+                        <div class="detail-akad">
+                            <div class="detail-time">
+                                <section>
+                                    <p>12.00 - Selesai</p>
+                                </section>
+                                <section>
+                                    <p>Wedding Party</p>
+                                </section>
+                            </div> <!-- end detail-time -->
+                            <div class="detail-date">
+                                <h1 class="font-weight-bold">MEI</h1>
+                                <h1 class="font-weight-bold">30</h1>
+                                <h1 class="font-weight-bold">2021</h1>
+                            </div> <!-- end detail-date -->
+                            <div class="detail-place">
+                                <section>
+                                    <p>Lap. Serbaguna</p>
+                                </section>
+                                <section>
+                                    <p>Bekasi</p>
+                                </section>
+                            </div> <!-- end detail-place -->
+                        </div> <!-- end detail-akad -->
+                    </div> <!-- end akad -->
+                </div> <!-- end schedule -->
+                <div class="location d-flex flex-column">
+                    <i class="fa fa-map-marker"></i>
+                    <div>Jl.Pesut Raya Lapangan serbaguna (Kantor RW 10) RT 01 RW 10 Perumnas 2 Kayuringin Jaya Bekasi Selatan</div>
+                </div> <!-- end location -->
+            </div> <!-- end schedule-container -->
+        </div> <!-- end #content -->
+    </main>
+    <footer>
+    </footer>
+</body>
+
+</html>
